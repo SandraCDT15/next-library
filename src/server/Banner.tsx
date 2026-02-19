80699a19cd13fec73e9511095e90c4a5f1f6cd08
Sandra Delgado <sandragullit@gmail.com>
2026-02-19 10:47:15 -0600

    Add banner and button component
    - Change components props and styles

--- src/server/Banner.tsx
+++ src/server/Banner.tsx
@@ -1,7 +1,23 @@
-export function Banner() {
+import { Button } from "../client/Button";
+
+interface BannerProps {
+  title: string;
+  description: string;
+  backgroundColor: string;
+  children: React.ReactNode;
+}
+
+export function Banner({
+  title,
+  description,
+  backgroundColor,
+  children,
+}: BannerProps) {
   return (
-    <div>
-      <h2>Hola</h2>
+    <div style={{ backgroundColor: backgroundColor }}>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      {children}
     </div>
   );
 }
